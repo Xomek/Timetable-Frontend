6e9230308c0e6c0bfc095832d6ec38f7eb4a3221
XomekX <z_v_m_x@mail.ru>
2022-06-03 16:02:54 +0700

    #1 implemented contextModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
-import { FC } from "react";
+import { Modal } from "@mui/material";
+import {
+  createContext,
+  Dispatch,
+  FC,
+  SetStateAction,
+  useState,
+  ReactElement,
+} from "react";
+import { Header } from "./components";
+
+export interface IContextModal {
+  setVisibleModal: Dispatch<SetStateAction<boolean>>;
+  setContentModal: Dispatch<SetStateAction<ReactElement>>;
+}
+
+export const contextModal = createContext<IContextModal>({} as IContextModal);
 
 const App: FC = () => {
-  return <></>;
+  const [isVisibleModal, setVisibleModal] = useState<boolean>(false);
+  const [contentModal, setContentModal] = useState<ReactElement>(<div />);
+
+  return (
+    <>
+      <Modal open={isVisibleModal} onClose={() => setVisibleModal(false)}>
+        {contentModal}
+      </Modal>
+      <contextModal.Provider
+        value={{
+          setVisibleModal,
+          setContentModal,
+        }}
+      >
+        <Header />
+      </contextModal.Provider>
+    </>
+  );
 };
 
 export default App;
