a89bab411f2ad83d376e009235567d39b4a3df25
XomekX <z_v_m_x@mail.ru>
2022-06-06 22:53:46 +0700

    #8 styled lesson component and created dots button for weeks

--- src/components/Weeks/Week/index.tsx
+++ src/components/Weeks/Week/index.tsx
@@ -25,25 +25,82 @@ const Week: FC<IWeekProps> = ({ week }) => {
   };
 
   return (
-    <Slider {...settings}>
-      {week.days.map((day) => (
-        <Card key={day.id} sx={{ height: "500px" }}>
-          <CardHeader sx={{ textAlign: "center" }} title={day.title} />
-          <CardContent>
-            {day.lessons.map((lesson) => (
-              <Box key={lesson.id}>
-                <Typography>{lesson.title}</Typography>
-                <Typography>{lesson.type}</Typography>
-                <Typography>{lesson.teacher}</Typography>
-                <Typography>{lesson.cabinetNumber}</Typography>
-                <Typography>{lesson.startDate}</Typography>
-                <Typography>{lesson.endDate}</Typography>
-              </Box>
-            ))}
-          </CardContent>
-        </Card>
-      ))}
-    </Slider>
+    <>
+      <Slider {...settings}>
+        {week.days.map((day) => (
+          <Card
+            key={day.id}
+            sx={{
+              height: "500px",
+              color: "#fff",
+              backgroundColor: "#e4e6e7",
+              overflowY: "auto",
+              "::-webkit-scrollbar": { display: "none" },
+            }}
+          >
+            <CardHeader
+              sx={{ textAlign: "center", color: "#2d333b" }}
+              title={day.title}
+            />
+            <CardContent>
+              {day.lessons.map((lesson) => (
+                <Box
+                  key={lesson.id}
+                  sx={{
+                    backgroundColor: "#2d333b",
+                    padding: "20px",
+                    borderRadius: "8px",
+                    marginBottom: "10px",
+                  }}
+                >
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <Typography>{lesson.title}</Typography>
+                    <Typography
+                      sx={{
+                        fontSize: "12px",
+                        fontWeight: "700",
+                      }}
+                    >
+                      {lesson.type}
+                    </Typography>
+                  </Box>
+
+                  <Typography sx={{ fontSize: "13px", mb: "10px" }}>
+                    {lesson.teacher}
+                  </Typography>
+                  <Box
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <Box
+                      sx={{
+                        display: "flex",
+                        fontSize: "12px",
+                      }}
+                    >
+                      <Typography sx={{ pr: "3px", fontSize: "12px" }}>
+                        {lesson.startDate}
+                      </Typography>
+                      <span>-</span>
+                      <Typography sx={{ pl: "3px", fontSize: "12px" }}>
+                        {lesson.endDate}
+                      </Typography>
+                    </Box>
+                    <Typography sx={{ fontSize: "12px" }}>
+                      Кабинет {lesson.cabinetNumber}
+                    </Typography>
+                  </Box>
+                </Box>
+              ))}
+            </CardContent>
+          </Card>
+        ))}
+      </Slider>
+    </>
   );
 };
 
--- src/components/Weeks/index.tsx
+++ src/components/Weeks/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, ReactNode, useRef, useState } from "react";
 import { IWeek } from "../../interfaces/week.interface";
 import Week from "./Week";
 import "slick-carousel/slick/slick.css";
@@ -6,29 +6,69 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { Settings } from "react-slick";
 import { Container } from "@mui/system";
+import { Box, Button } from "@mui/material";
 
 interface IWeeksProps {
   weeks: IWeek[];
 }
 
 const Weeks: FC<IWeeksProps> = ({ weeks }) => {
+  const [activeButton, setActiveButton] = useState<boolean>(true);
+
   const settings: Settings = {
-    dots: true,
-    infinite: true,
+    infinite: false,
     arrows: false,
     speed: 100,
     slidesToShow: 1,
     slidesToScroll: 1,
     swipe: false,
+    appendDots: appendDots,
   };
+
+  const sliderRef = useRef<any>(null);
+
+  function appendDots(dots: ReactNode[]) {
+    return <ul className="week-dots">{dots.map((dot) => dot)}</ul>;
+  }
 
   return (
     <Container>
-      <Slider {...settings}>
+      <Slider ref={sliderRef} {...settings}>
         {weeks.map((week) => (
           <Week key={week.id} week={week} />
         ))}
       </Slider>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        {appendDots([
+          <Button
+            variant={activeButton ? "contained" : "outlined"}
+            sx={{
+              color: "#fff",
+              mr: "5px",
+              borderColor: "#1e202a",
+            }}
+            onClick={() => {
+              sliderRef.current.slickPrev();
+              setActiveButton(true);
+            }}
+          >
+            Четная
+          </Button>,
+          <Button
+            variant={activeButton ? "outlined" : "contained"}
+            sx={{
+              color: "#fff",
+              borderColor: "#1e202a",
+            }}
+            onClick={() => {
+              sliderRef.current.slickNext();
+              setActiveButton(false);
+            }}
+          >
+            Нечетная
+          </Button>,
+        ])}
+      </Box>
     </Container>
   );
 };
