1ca176d7f5727ea3e92889b7474da5e6185b4a3e
XomekX <z_v_m_x@mail.ru>
2022-06-08 22:13:18 +0700

    #11 created component Settings and show him and modal

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import LoginForm from "./LoginForm";
 import RegistrationForm from "./RegistrationForm";
 import Group from "./Group";
 import Weeks from "./Weeks";
+import Settings from "./Settings";
 
 export { Header, LoginForm, RegistrationForm, Group, Weeks };
