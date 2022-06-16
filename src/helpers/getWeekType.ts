180e12ca6ea2fdbedc8171bd1f8595d193a4005f
XomekX <z_v_m_x@mail.ru>
2022-06-16 21:04:42 +0700

    #14 created getWeekType helper

---
+++ src/helpers/getWeekType.ts
@@ -0,0 +1,8 @@
+export const getWeekType = () => {
+  let d0 = new Date().getTime(),
+    d = new Date(new Date().getFullYear(), 0, 1),
+    d1 = d.getTime(),
+    dd = d.getDay(),
+    re = Math.floor((d0 - d1) / 8.64e7) + (dd ? dd - 1 : 6);
+  return Math.floor(re / 7) % 2 ? 1 : 0;
+};
